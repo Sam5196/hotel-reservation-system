f5ef745e42202a4cf7ae8aa327755885929b1934
Sam5196 <sammedpachore1008@gmail.com>
2022-08-09 14:30:22 +0530

    Added:Logic to add ratings to each hotel

--- src/main/java/com/bridgelabz/HotelReservationMain.java
+++ src/main/java/com/bridgelabz/HotelReservationMain.java
@@ -5,7 +5,7 @@ import java.time.Month;
 
 public class HotelReservationMain {
     public static void main(String[] args) {
-        System.out.println(" Welcome To Hotel Reservation System");
+        System.out.println(" Welcome To Hotel Reservation System......!");
 
         HotelReservation hotelReservation = new HotelReservation();
         hotelReservation.addHotel();
