74d0cd9b673e56c20202142464e5db722e037729
Sam5196 <sammedpachore1008@gmail.com>
2022-08-09 13:47:15 +0530

    Added:Logic to add hotel

--- src/main/java/com/bridgelabz/HotelReservationMain.java
+++ src/main/java/com/bridgelabz/HotelReservationMain.java
@@ -2,9 +2,8 @@ package com.bridgelabz;
 
 public class HotelReservationMain {
     public static void main(String[] args) {
-        System.out.println(" Welcome To Hotel Reservation System");
+        System.out.println(" Welcome To Hotel Reservation System........!");
         /**
-         * Creating instance of Hotel Reservation - hotelReservation Calling method
          * addHotel for this instance(hotelReservation) Calling method printHotelList to
          * print created hotelList
          */
